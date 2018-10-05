e2642f59ba5982c92c935cc6231bb815a3dcaaa9
Voodoobear <s.chalain@gmail.com>
2018-10-05 17:55:41 +0200

    Ajustements mineurs, tests de clustering

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Création et initialisation de la carte
-var mymap = L.map('mapid').setView([45.767752, 4.835711], 13);
+var mymap = L.map('mapid').setView([45.767752, 4.835711], 12);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -12,11 +12,12 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 var redIcon = L.icon({
     iconUrl: './img/marker.png',
 
-    iconSize:     [30, 48], // size of the icon
-    iconAnchor:   [45, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconSize:     [25, 40], // taille
+    iconAnchor:   [45, 94], // localisation
+    shadowAnchor: [4, 62],  // shadow localisation
+    popupAnchor:  [-3, -76] // point d'initialisation de la pop-up
 });
+
 var marker = L.marker([45.767752, 4.835711], {icon: redIcon}).addTo(mymap);
 
 // Récupération des données de l'API JCDeaux
@@ -26,6 +27,13 @@ ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=4a546114c
       velovJSON.forEach(function(velovJSON) {
         console.log(velovJSON.number, velovJSON.name + "\n", velovJSON.position.lat, velovJSON.position.lng, velovJSON.address),
         // utilisation des données de position pour afficher cahque markers au positions des stations velov
-        new L.marker([velovJSON.position.lat, velovJSON.position.lng], {icon: redIcon}).addTo(mymap);
+        new L.marker([velovJSON.position.lat, velovJSON.position.lng], {icon: redIcon}).addTo(mymap),
+        marker.bindPopup(velovJSON.name + " , " + velovJSON.address).openPopup();
+        
+        
+        
       })
     });
+
+
+    
